ff7d50c6c0248abfcb2a3e3575e26ed80389bfac
shardul bhostekar <shardulbhosteka15@gmail.comr>
2025-06-05 10:31:57 +0530

    sh

--- hello.js
+++ hello.js
@@ -1,8 +1,15 @@
-console.log("hello world");
-let number=10;
-if(number%2==0){
-    console.log("even");
-}else{
-    console.log("odd");
-}
-let num=23;
+// console.log("hello world");
+// let number=10;
+// if(number%2==0){
+//     console.log("even");
+// }else{
+//     console.log("odd");
+// }
+// let num=23;
+// const prompt = require ('prompt-sync')();
+// const name = prompt("what is your name? ");
+// console.log ("hello," " + name +"!");
+    function square (num) {
+        return num*num
+    }
+    console.log(square(1000))
